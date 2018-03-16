0e881c5fdd3aea71c33b50d3bd5b990f19e6d736
Rafael Magalhães <rafaelmm82@gmail.com>
2018-03-16 10:52:13 -0300

    another lines

--- datacamp/case_study_hacker_statistics.py
+++ datacamp/case_study_hacker_statistics.py
@@ -1,11 +1,14 @@
 """
 Application of initial functions, packages and libraries associated with
-datascience using python
+datascience using python, course:
 
+"Intermediate Python for Data Science"
+
+This project concerns on simulate a random walk through the floors stages on Empire Building State,
 """
 
 # Importing the essentials
-import numpy as numpy
+import numpy as np
 
 # setting the initial seed to random numbers
 np.random.seed(123)
