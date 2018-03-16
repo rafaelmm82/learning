16798a8587e296353c85b65e707cb7a389804b44
Rafael Magalhães <rafaelmm82@gmail.com>
2018-03-16 10:36:48 -0300

    add of datacamp directory
    a new subproject in learning

---
+++ datacamp/case_study_hacker_statistics.py
@@ -0,0 +1,10 @@
+"""
+Application of initial functions, packages and libraries associated with
+datascience using python
+
+"""
+
+# Importing the essentials
+import numpy as numpy
+
+# setting the initial seed to random numbers
