00833305dd0441e089282a8e2721e9a8758b7adc
Rafael Magalhães <rafaelmm82@gmail.com>
2019-03-17 19:01:49 +0100

    Partie 1
    Importer les librairies
    Importer le dataset
    Gérer les données manquantes

---
+++ mlfrancais/Partie 1 - Data Preprocessing/data_preprocessing.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 17 18:03:24 2019
+
+@author: @rafaelmm82
+github: @rafaelmm82
+
+Les codes dans cettes scripts font partie de mon apprentissage du français.
+J'ai décidé de commencer un cours  de ma compétence technique pour faciliter
+l'assimilation du vocabulaire et de la structure de la langue française.
+
+
+Partie 1 - Data-preprocessing
+
+"""
+
+# Importer les librairies
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.impute import SimpleImputer
+
+# Importer le dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
+
+# Gérer les données manquantes
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+
+
---
+++ mlfrancais/README.md
@@ -0,0 +1,4 @@
+Les codes dans cettes scripts font partie de mon apprentissage du français.
+J'ai décidé de commencer un cours  de ma compétence technique pour faciliter
+l'assimilation du vocabulaire et de la structure de la langue française.
+
