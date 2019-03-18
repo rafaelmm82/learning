46de6de7d7a0c4adedb8de55c3dd8e4e38cf5796
Rafael Magalhaes <rafaelmm82@gmail.com>
2019-03-18 18:34:27 +0100

    part 6 after my account view

--- sbtc-django/myproject/boards/views.py
+++ sbtc-django/myproject/boards/views.py
@@ -27,24 +27,25 @@ class BoardListView(ListView):
 #     boards = Board.objects.all()
 #     return render(request, 'home.html', {'boards': boards})
 
-def board_topics(request, pk):
-    board = get_object_or_404(Board, pk=pk)
-    queryset = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
-    page = request.GET.get('page', 1)
 
-    paginator = Paginator(queryset, 20)
+# def board_topics(request, pk):
+#     board = get_object_or_404(Board, pk=pk)
+#     queryset = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+#     page = request.GET.get('page', 1)
 
-    try:
-        topics = paginator.page(page)
-    except PageNotAnInteger:
-        # fallback to the first page
-        topics = paginator.page(1)
-    except EmptyPage:
-        # probably the user tried to add page number
-        # in the url, so we fallback to the last page
-        topics = paginator.page(paginator.num_pages)
+#     paginator = Paginator(queryset, 20)
+
+#     try:
+#         topics = paginator.page(page)
+#     except PageNotAnInteger:
+#         # fallback to the first page
+#         topics = paginator.page(1)
+#     except EmptyPage:
+#         # probably the user tried to add page number
+#         # in the url, so we fallback to the last page
+#         topics = paginator.page(paginator.num_pages)
     
-    return render(request, 'topics.html', {'board': board, 'topics': topics})
+#     return render(request, 'topics.html', {'board': board, 'topics': topics})
 
 
 
@@ -71,11 +72,11 @@ def new_topic(request, pk):
     
     return render(request, 'new_topic.html', {'board': board, 'form': form})
 
-def topic_posts(request, pk, topic_pk):
-    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
-    topic.views += 1
-    topic.save()
-    return render(request, 'topic_posts.html', {'topic': topic})
+# def topic_posts(request, pk, topic_pk):
+#     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+#     topic.views += 1
+#     topic.save()
+#     return render(request, 'topic_posts.html', {'topic': topic})
 
 
 @login_required
@@ -117,3 +118,38 @@ class PostUpdateView(UpdateView):
         post.save()
         return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
 
+
+class TopicListView(ListView):
+
+    model = Topic
+    context_object_name = 'topics'
+    template_name = 'topics.html'
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        kwargs['board'] = self.board
+        return super().get_context_data(**kwargs)
+
+    def get_queryset(self):
+        self.board = get_object_or_404(Board, pk=self.kwargs.get('pk'))
+        queryset = self.board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+        return queryset
+
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'topic_posts.html'
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+        self.topic.views += 1
+        self.topic.save()
+        kwargs['topic'] = self.topic
+        return super().get_context_data(**kwargs)
+    
+    def get_queryset(self):
+        self.topic = get_object_or_404(Topic, board__pk=self.kwargs.get('pk'), pk=self.kwargs.get('topic_pk'))
+        queryset = self.topic.posts.order_by('created_at')
+        return queryset
+    
