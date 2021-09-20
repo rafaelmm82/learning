360675e19f440e428c1258c3b5a2733b1f502948
Rafael Magalhães <rafaelmm82@gmail.com>
2021-09-20 17:20:49 -0300

    gitignore python

---
+++ mlfrancais/Partie 1 - Data Preprocessing/01 - Data Preprocessing.py
@@ -0,0 +1,12 @@
+'''
+cours en france de machine learning
+Github: @rafaelmm82
+'''
+
+# Importer les librairies
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importer le dataset
+dataset = pd.read_csv('Data.csv')
