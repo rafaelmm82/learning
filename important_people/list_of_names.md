c8cd7408404dbbcc0af3ae0a18c6c311d4003269
rafaelmm82 <rafaelmm82@gmail.com>
2022-05-17 12:51:40 -0300

    starting pstatistical for ds book

---
+++ important_people/list_of_names.md
@@ -0,0 +1,1 @@
+- John Tukey, statistics, EDA, bit, software, algorithms
---
+++ pstatistics_ds/ch_01 - Exploratory Data Analysis.py
@@ -0,0 +1,15 @@
+# Notes on Exploratory Data Analysis chapter
+# -----------------------------------------
+
+# To apply the statistical concepts covered in this book, unstructured raw 
+# data must be processed and manipulated into a structured form.
+# One of the commonest forms of structured data is a table with rows and 
+# columns—as data might emerge from a relational database or be collected 
+# for a study.
+
+# Types of data: 
+# Unstructured data: raw data that has not been processed into a table.
+# Structured data: data that has been processed into a table.
+# - Numerical (continuous or discrete), 
+# - Categorical (fixed set, binary [true/false], ordinal)
+
